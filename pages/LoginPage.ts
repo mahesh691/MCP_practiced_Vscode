303861f7a85deacf37e9673e0686ba77a0341de6
mahesh691 <55076400+mahesh691@users.noreply.github.com>
2026-09-19 10:34:27 +0530

    Update login selectors in LoginPage

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -8,8 +8,8 @@ export class LoginPage {
   }
 
   async login(username: string, password: string) {
-    await this.page.fill('input[name="username"]', username);
-    await this.page.fill('input[name="password"]', password);
-    await this.page.click('input[value="Log In"]');
+     await this.page.fill('#user-name', username);
+    await this.page.fill('#password', password);
+    await this.page.click('#login-button');
   }
 }
