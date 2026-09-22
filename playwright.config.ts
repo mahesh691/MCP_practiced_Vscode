6bd3775d1d1c05b5af508cdf59dadae6be092b72
mahesh691 <55076400+mahesh691@users.noreply.github.com>
2026-09-22 20:45:44 +0530

    Comment out dotenv setup in playwright.config.ts
    Comment out dotenv import and configuration for dependency installation.

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, 'tests/.env') });
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, 'tests/.env') }); Need to install the dependencies
 
 /**
  * See https://playwright.dev/docs/test-configuration.
